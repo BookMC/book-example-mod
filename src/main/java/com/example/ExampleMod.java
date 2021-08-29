199acfb87ce5071035e02445994bab696c5e0587
ChachyDev <shalomadecoolboy@outlook.com>
2021-08-29 21:43:53 +0100

    New example mod

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -3,10 +3,13 @@ package com.example;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+/**
+ * This the common area of your code. You don't have access to Minecraft but Minecraft has access to you
+ */
 public class ExampleMod {
-    private final Logger logger = LogManager.getLogger(this);
-
     public static ExampleMod INSTANCE = new ExampleMod();
+
+    private final Logger logger = LogManager.getLogger();
 
     public void main() {
         logger.info("ExampleMod was loaded! :)");
