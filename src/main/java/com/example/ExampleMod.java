e75f5ee625218678efeed81aa3c6b84cd84be618
ChachyDev <shalomadecoolboy@outlook.com>
2021-09-03 15:21:44 +0100

    Update artifacts and documentation

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -3,9 +3,6 @@ package com.example;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-/**
- * This the common area of your code. You don't have access to Minecraft but Minecraft has access to you
- */
 public class ExampleMod {
     public static ExampleMod INSTANCE = new ExampleMod();
 
